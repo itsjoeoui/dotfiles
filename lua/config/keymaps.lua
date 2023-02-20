f028f276beb030cca3c6f0069f976d9e222dea6b
Joey Yu <joey@itsjoeoui.com>
2023-02-19 22:43:05 -0500

    Basic configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<A-j>", "j")
+vim.keymap.set("i", "<A-k>", "k")
+vim.keymap.set("v", "<A-j>", "j")
+vim.keymap.set("v", "<A-k>", "k")
+vim.keymap.set("n", "<A-j>", "j")
+vim.keymap.set("n", "<A-k>", "k")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      sources = {
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.black,
+      },
+    }
+  end,
+}
