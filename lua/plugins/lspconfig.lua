aee81d95c62d4636684c1ca9b12eb63c1bb35c14
Joey Yu <joey@itsjoeoui.com>
2023-05-05 13:09:16 -0400

    Update config
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -26,19 +26,19 @@ return {
         },
       },
     },
-  },
-  setup = {
-    tsserver = function(_, opts)
-      require("lazyvim.util").on_attach(function(client, buffer)
-        if client.name == "tsserver" then
+    setup = {
+      tsserver = function(_, opts)
+        require("lazyvim.util").on_attach(function(client, buffer)
+          if client.name == "tsserver" then
           -- stylua: ignore
           vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
           -- stylua: ignore
           vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-        end
-      end)
-      require("typescript").setup({ server = opts })
-      return true
-    end,
+          end
+        end)
+        require("typescript").setup({ server = opts })
+        return true
+      end,
+    },
   },
 }
