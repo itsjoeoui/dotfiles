f9187b6cb4a5c7952dc9b013c9357c64c9cd1bcc
Joey Yu <joey@itsjoeoui.com>
2023-05-05 18:03:21 -0400

    Display hex color in nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "<A-j>", "j")
-vim.keymap.set("i", "<A-k>", "k")
-vim.keymap.set("v", "<A-j>", "j")
-vim.keymap.set("v", "<A-k>", "k")
-vim.keymap.set("n", "<A-j>", "j")
-vim.keymap.set("n", "<A-k>", "k")
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "J", "mzJ`z")
+map("i", "<A-j>", "j")
+map("i", "<A-k>", "k")
+map("v", "<A-j>", "j")
+map("v", "<A-k>", "k")
+map("n", "<A-j>", "j")
+map("n", "<A-k>", "k")
+
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "J", "mzJ`z")
+
+map("n", "<leader>_", "<C-W>s", { desc = "Split window below" })
