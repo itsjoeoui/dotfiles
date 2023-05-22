d45631d089c096a0babde1ec6745674eb1c9e781
Joey Yu <joey@itsjoeoui.com>
2023-05-22 10:23:47 -0400

    fuck jupyter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,6 +17,7 @@ return {
       layout_config = { prompt_position = "top" },
       sorting_strategy = "ascending",
       winblend = 0,
+      file_ignore_patterns = { ".ipynb" },
     },
   },
 }
