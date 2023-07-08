e411d44595a5527f0c2c0e0c40b7c58ed9ed3b57
Joey Yu <joey@itsjoeoui.com>
2023-07-07 22:11:18 -0400

    Update
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.go" },
