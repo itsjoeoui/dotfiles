139336eb71c2a607f20927d4e3ccb4c4d1cbce94
Joey Yu <joey@itsjoeoui.com>
2023-07-21 13:00:28 -0400

    Update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,16 +11,18 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,12 +5,10 @@ return {
     return {
       sources = {
         nls.builtins.formatting.stylua,
-        nls.builtins.formatting.rustfmt,
         nls.builtins.formatting.black,
         nls.builtins.formatting.ocamlformat,
         nls.builtins.formatting.prettierd,
         nls.builtins.formatting.fourmolu,
-        nls.builtins.diagnostics.ruff,
         nls.builtins.code_actions.eslint_d,
         require("typescript.extensions.null-ls.code-actions"),
       },
