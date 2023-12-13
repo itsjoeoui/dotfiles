850807f096de5eeb07acc88d6503d51e881fda00
Joey Yu <joey@jyu.dev>
2023-12-12 23:30:53 -0500

    feat: tailwind intellisnese

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,6 +5,14 @@ return {
       enabled = false,
     },
     servers = {
+      tailwindcss = {
+        filetypes = { "templ" },
+        init_options = {
+          userLanguages = {
+            templ = "html",
+          },
+        },
+      },
       eslint = {
         settings = {
           -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
