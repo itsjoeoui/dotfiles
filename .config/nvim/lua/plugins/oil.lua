1db938c48b202ddc35bfc4c8d64590316070b104
Joey Yu <joey@jyu.dev>
2024-10-06 12:03:27 -0400

    feat(nvim): add oil and update

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = false,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
