a0a39a96155e44e3083ecdf36daca0e5fb28a944
Joey Yu <joey@jyu.dev>
2025-01-09 10:20:23 -0500

    chore: shopify

--- .config/nvim/lua/plugins/wakatime.lua
+++ .config/nvim/lua/plugins/wakatime.lua
@@ -1,3 +1,9 @@
-return {
-  "wakatime/vim-wakatime",
-}
+local hostname = vim.fn.hostname()
+
+if hostname == "Joeys-Shopify-MacBook-Pro.local" then
+  return {}
+else
+  return {
+    "wakatime/vim-wakatime",
+  }
+end
