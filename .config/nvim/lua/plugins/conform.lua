ad8729af4a6d7539a4281fd67a34c2c481b29a67
Joey Yu <joey@jyu.dev>
2025-02-21 15:28:06 -0500

    chore: update

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,12 +3,12 @@ return {
   opts = {
     formatters_by_ft = {
       -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      json = { "prettierd", "prettier" },
-      markdown = { "prettierd", "prettier" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier", stop_after_first = true },
 
       templ = { "templ" },
 
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,5 +3,16 @@ return {
   ---@type snacks.Config
   opts = {
     notifier = { enabled = true, top_down = false },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
   },
 }
