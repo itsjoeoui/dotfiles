1945d6ee00bb955858a73ee5770dbe4c17358f01
Joey Yu <joey@jyu.dev>
2025-04-09 14:08:24 -0400

    chore: register excalidraw

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -2,8 +2,10 @@ vim.filetype.add({
   extension = {
     mdx = "mdx",
     ejson = "ejson",
+    excalidraw = "excalidraw",
   },
 })
 
 vim.treesitter.language.register("markdown", "mdx")
 vim.treesitter.language.register("json", "ejson")
+vim.treesitter.language.register("json", "excalidraw")
