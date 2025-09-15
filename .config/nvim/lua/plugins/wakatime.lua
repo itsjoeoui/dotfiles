3d11d3645f5ce6e7ce6069cb5c9c345b6d558361
Joey Yu <itsjoeoui@Joeys-MacBook-Pro.local>
2025-09-15 10:47:10 -0400

    disable wakatime

--- .config/nvim/lua/plugins/wakatime.lua
+++ .config/nvim/lua/plugins/wakatime.lua
@@ -1,9 +1,9 @@
-local hostname = vim.fn.hostname()
-
-if hostname == "Joeys-Shopify-MacBook-Pro.local" then
-  return {}
-else
-  return {
-    "wakatime/vim-wakatime",
-  }
-end
+-- local hostname = vim.fn.hostname()
+--
+-- if hostname == "Joeys-Shopify-MacBook-Pro.local" then
+--   return {}
+-- else
+--   return {
+--     "wakatime/vim-wakatime",
+--   }
+-- end
