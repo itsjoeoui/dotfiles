ea3b3ed17cd15cb86961bb438162f9b00d1013e4
Joey Yu <joey@jyu.dev>
2025-09-17 17:13:57 -0400

    rename

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -9,6 +9,6 @@ return {
     },
   },
   -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
