33f84ea69ec236bcd6119efc4a0e2d24f52bc744
Joey Yu <joey@jyu.dev>
2025-09-17 15:04:05 -0400

    nuke wakatime

--- .config/nvim/lua/plugins/wakatime.lua
+++
@@ -1,9 +0,0 @@
--- local hostname = vim.fn.hostname()
---
--- if hostname == "Joeys-Shopify-MacBook-Pro.local" then
---   return {}
--- else
---   return {
---     "wakatime/vim-wakatime",
---   }
--- end
